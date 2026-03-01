5e155ecc03e79e8cc9f979d912f25ad71e5be518
PRAJWAL SHIVASHIMPAR <prajwal.12smp@gmail.com>
2026-03-01 15:23:30 +0530

    added basics

---
+++ Basics/First program.py
@@ -0,0 +1,58 @@
+# This is a simple Python program that prints "Hello, World!" to the console.
+print("Hello, World!")
+
+# This program demonstrates basic string output in Python.
+print("Welcome to Python programming! Prajwal ")
+print("My age is 21 years old.")
+
+# You can also combine multiple strings in a single print statement.
+print("Prajwal is my name", "My age is 21 years old")
+
+# You can print numbers as well.
+print(23)
+print(35)
+
+# You can perform basic arithmetic operations in Python.
+
+print(23 + 35) # Addition
+print(23 - 35) # Subtraction
+print(23 * 35) # Multiplication
+print(23 / 35) # Division (results in a float)
+print(23 % 35) # Modulus (remainder of the division)
+print(23 ** 35) # Exponentiation (23 raised to the power of 35)
+print(23 // 35) # Floor division (results in the largest integer less than or equal to the division result)
+print(23 + 35 * 2) # Order of operations (multiplication before addition)
+
+# You can also use variables to store values and perform operations on them.
+name = "Prajwal Shivashimpar" # String variable to store a name
+age = 21 # Integer variable to store age
+price = 19.99 # Float variable to store price
+print(name)
+print(age)
+print(price)
+print("My name is :", name)
+print("My age is :", age)
+print("The price of the item is :", price)
+
+#Rules for Identifiers in Python:
+# 1. An identifier can only contain letters (a-z, A-Z), digits (0-9), and underscores (_).
+# 2. An identifier cannot start with a digit.
+# 3. An identifier cannot be a reserved keyword in Python (e.g., if, else, while, for, etc.).
+# 4. An identifier is case-sensitive (e.g., myVariable and MyVariable are different).
+# 5. An identifier should not contain spaces (use underscores instead).
+# 6. Identifiers can be of any length, but it is recommended to keep them concise and meaningful.
+# 7. we cannot use special characters like @, #, $, etc. in identifiers.
+
+#Data types in python:
+# 1. int: Represents integer values (e.g., 1, -5, 0).
+# 2. float: Represents floating-point numbers (e.g., 3.14, -0.5).
+# 3. str: Represents strings, which are sequences of characters (e.g., "Hello", 'Python').
+# 4. bool: Represents boolean values, which can be either True or False.
+# 5. none: Represents the absence of a value or a null value (e.g., None).
+
+name = "Prajwal Shivashimpar" 
+age = 21 
+price = 19.99
+print(type(name)) # Output: <class 'str'>
+print(type(age)) # Output: <class 'int'>
+print(type(price)) # Output: <class 'float'>
