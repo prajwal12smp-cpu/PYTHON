7d2d6f920f75cfa7e19013559ab94d1d6df82f15
PRAJWAL SHIVASHIMPAR <prajwal.12smp@gmail.com>
2026-03-02 20:08:32 +0530

    added strings

---
+++ Strings & Conditional Statements/Strings.py
@@ -0,0 +1,21 @@
+#strings are a sequence of characters enclosed in single, double or triple quotes
+str1 = "this is a string"
+str2 = 'Prajwal'
+str3 = """This is a multi line string"""
+print(str1)
+print(str2)
+print(str3)
+
+#string formatting using escape characters
+str1 = "This is a string.\n we are creating it in python."
+print(str1)
+
+#string concatenation
+str4 = "Prajwal"
+str5 = "Shivashimpar"
+print(str4 + " " + str5)
+
+#string length
+print(len(str4))
+print(len(str5))
+print(len(str4 + " " + str5))
