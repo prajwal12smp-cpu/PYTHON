ddc01cfcc9e36a4ea82041a89bc33c4aaa2ec561
PRAJWAL SHIVASHIMPAR <prajwal.12smp@gmail.com>
2026-03-06 15:39:11 +0530

    added practice.py

---
+++ Strings & Conditional Statements/practice.py
@@ -0,0 +1,9 @@
+#write a program to input user's first name & print its length.
+first_name = input("Enter your first name: ")
+length = len(first_name)
+print("The length of your first name is:", length)
+
+
+#write a program to find the occurrence of '$' in a string.
+str = "Hi, $Iam the $ symbol $99.99"
+print("The occurrence of '$' in the string is:", str.count('$'))
